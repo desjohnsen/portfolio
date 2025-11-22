0eb89c29e567b71e97c8b197fafdcce43dd2905a
desjohnsen <des.johnsen@gmail.com>
2025-11-22 19:36:47 +0100

    chore: delete unused profile image

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "../styles/About.css";
 import profilePicDesktop from "../assets/images/polaroid.jpg";
-import profilePicMobile from "../assets/images/profilepic2.jpg";
+import profilePicMobile from "../assets/images/profilepic.jpg";
 
 const SkillCard = ({ front, back }) => {
   const [flipped, setFlipped] = React.useState(false);
